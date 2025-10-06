53c2cf53f7708f997e9ce2a502546abd12bd6ebb
Uzziel Palma <uzzielpalma99@gmail.com>
2025-10-06 16:22:52 -0600

    Production VN 1.0.0 BN 36

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,65 +1,5 @@
 library;
-// TEST Services List
-
-/// <<<<<<<< Embebed Forms Web >>>>>>>>>
-///
-const String urlBaseEmbed = 'https://www.freeway.com/';
-
-const String urlBaseEmbedBuyProduct = 'https://buy.freeway.com/product/';
-
-const String urlBaseEmbedCarRegistration = 'https://www.carregistration.com/';
-
-const String urlBaseEmbedTriton = 'https://triton.freeway.com/';
-
-const String urlBaseEmbedTaxmax = 'https://www.taxmax.com/';
-
-const String urlBaseEmbedRate = 'https://rate.freeway.com/';
-
-const String urlBaseEmbedQuote = 'https://quote.sanborns.com/';
-
-const String urlBaseEmbedSeguros = 'https://www.freewayseguros.com/';
-
-const String urlBaseEmbedQuickPay = 'https://quickpay.freeway.com/';
-
-/// <<<<<<<< User Profile & Policy >>>>>>>>>
-/// <<<<<<<< Login & Registration >>>>>>>>>
-
-const String envLogin = String.fromEnvironment(
-  'env',
-  defaultValue: 'https://confie-customer-np.azurewebsites.net',
-);
-
-const String apiKeyLogin =
-    'jEk40pLbflj4vQ6RyhQmI3JxDAXjUhdWrEjYBgQRAuSs8X6ged161peEtM4mM8sT';
-
-/// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
-
-const String envWallet = String.fromEnvironment(
-  'env',
-  defaultValue: 'https://confie-wallet-api-np.azurewebsites.net',
-);
-
-const String apiKeyWallet = 'GfhGdjdx3rfGBBFkf';
-
-/// <<<<<<<< Office Locations >>>>>>>>>
-
-const String envOffice = String.fromEnvironment(
-  'env',
-  defaultValue: 'https://stg-inquiry.confie.com',
-);
-
-const String apiKeyOffice = 'fjzzkOuCefd8-Z86i9HMGWQ=';
-
-/// <<<<<<<< Offices Map >>>>>>>>>
-/// <<<<<<<< Thirds Party >>>>>>>>>
-
-const String envThirdsPartyZipcode = 'https://api.zippopotam.us/us/';
-
-const String envThirdsPartyAppleMap = 'https://maps.apple.com/';
-
-const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
-
-// // PROD Services List
+// // TEST Services List
 
 // /// <<<<<<<< Embebed Forms Web >>>>>>>>>
 // ///
@@ -86,29 +26,29 @@ const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
 
 // const String envLogin = String.fromEnvironment(
 //   'env',
-//   defaultValue: 'https://confie-customer.azurewebsites.net',
+//   defaultValue: 'https://confie-customer-np.azurewebsites.net',
 // );
 
 // const String apiKeyLogin =
-//     'TMDpw6vDVv5AJ2vGaMoQybFsZpm57U5BqaYhMGjf5WHYyys82huZYLRb1FN8r5Y6';
+//     'jEk40pLbflj4vQ6RyhQmI3JxDAXjUhdWrEjYBgQRAuSs8X6ged161peEtM4mM8sT';
 
 // /// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
 
 // const String envWallet = String.fromEnvironment(
 //   'env',
-//   defaultValue: 'https://confie-wallet-api.azurewebsites.net',
+//   defaultValue: 'https://confie-wallet-api-np.azurewebsites.net',
 // );
 
-// const String apiKeyWallet = 'Hwsed7698FdhskG5lkkg';
+// const String apiKeyWallet = 'GfhGdjdx3rfGBBFkf';
 
 // /// <<<<<<<< Office Locations >>>>>>>>>
 
 // const String envOffice = String.fromEnvironment(
 //   'env',
-//   defaultValue: 'https://inquiry.confie.com',
+//   defaultValue: 'https://stg-inquiry.confie.com',
 // );
 
-// const String apiKeyOffice = '0yoZaSdIgj+i+ny4+1TBvw==';
+// const String apiKeyOffice = 'fjzzkOuCefd8-Z86i9HMGWQ=';
 
 // /// <<<<<<<< Offices Map >>>>>>>>>
 // /// <<<<<<<< Thirds Party >>>>>>>>>
@@ -118,3 +58,63 @@ const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
 // const String envThirdsPartyAppleMap = 'https://maps.apple.com/';
 
 // const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
+
+// // PROD Services List
+
+/// <<<<<<<< Embebed Forms Web >>>>>>>>>
+///
+const String urlBaseEmbed = 'https://www.freeway.com/';
+
+const String urlBaseEmbedBuyProduct = 'https://buy.freeway.com/product/';
+
+const String urlBaseEmbedCarRegistration = 'https://www.carregistration.com/';
+
+const String urlBaseEmbedTriton = 'https://triton.freeway.com/';
+
+const String urlBaseEmbedTaxmax = 'https://www.taxmax.com/';
+
+const String urlBaseEmbedRate = 'https://rate.freeway.com/';
+
+const String urlBaseEmbedQuote = 'https://quote.sanborns.com/';
+
+const String urlBaseEmbedSeguros = 'https://www.freewayseguros.com/';
+
+const String urlBaseEmbedQuickPay = 'https://quickpay.freeway.com/';
+
+/// <<<<<<<< User Profile & Policy >>>>>>>>>
+/// <<<<<<<< Login & Registration >>>>>>>>>
+
+const String envLogin = String.fromEnvironment(
+  'env',
+  defaultValue: 'https://confie-customer.azurewebsites.net',
+);
+
+const String apiKeyLogin =
+    'TMDpw6vDVv5AJ2vGaMoQybFsZpm57U5BqaYhMGjf5WHYyys82huZYLRb1FN8r5Y6';
+
+/// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
+
+const String envWallet = String.fromEnvironment(
+  'env',
+  defaultValue: 'https://confie-wallet-api.azurewebsites.net',
+);
+
+const String apiKeyWallet = 'Hwsed7698FdhskG5lkkg';
+
+/// <<<<<<<< Office Locations >>>>>>>>>
+
+const String envOffice = String.fromEnvironment(
+  'env',
+  defaultValue: 'https://inquiry.confie.com',
+);
+
+const String apiKeyOffice = '0yoZaSdIgj+i+ny4+1TBvw==';
+
+/// <<<<<<<< Offices Map >>>>>>>>>
+/// <<<<<<<< Thirds Party >>>>>>>>>
+
+const String envThirdsPartyZipcode = 'https://api.zippopotam.us/us/';
+
+const String envThirdsPartyAppleMap = 'https://maps.apple.com/';
+
+const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
