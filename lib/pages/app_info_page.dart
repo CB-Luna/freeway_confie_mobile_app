7d1c2700fd139aaf3fbe7cd8a7f38a59dbbdf979
Uzziel Palma <uzzielpalma99@gmail.com>
2025-10-23 11:46:04 -0600

    Version Number 42 PROD

--- lib/pages/app_info_page.dart
+++ lib/pages/app_info_page.dart
@@ -106,8 +106,8 @@ class _AppInfoPageState extends State<AppInfoPage> {
   Widget _buildAppInfoCard(BuildContext context) {
     // Información de la aplicación
     const appVersion = '1.0.0';
-    const buildNumber = '41';
-    final buildDate = '10/21/2025';
+    const buildNumber = '42';
+    final buildDate = '10/23/2025';
 
     return Card(
       elevation: 2,
