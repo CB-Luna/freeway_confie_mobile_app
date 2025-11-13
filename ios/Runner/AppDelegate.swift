aa58df761dc22264e4398d3c5c44f64fa116aad2
Uzziel Palma <uzzielpalma99@gmail.com>
2025-11-13 16:13:18 -0600

    Se integra Firebase a la App

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,10 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Configure Firebase
+    FirebaseApp.configure()
+    
+    // Configure Google Maps
     GMSServices.provideAPIKey("AIzaSyB6C-S87hsklhvinf5X1czu3xdz52s2bBc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
